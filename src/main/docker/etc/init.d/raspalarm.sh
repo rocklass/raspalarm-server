d878ae01a5133e83bf76f300de9450671603f55f
pbrochado <pbrochado@cityway.fr>
2017-06-23 13:14:53 +0200

    Create user raspalarm in container

--- src/main/docker/etc/init.d/raspalarm.sh
+++ src/main/docker/etc/init.d/raspalarm.sh
@@ -4,6 +4,10 @@ name=raspalarm
 
 start() {
         ebegin "Starting $name"
+        sudo /etc/init.d/lighttpd start
+        sudo /etc/init.d/mariadb start
+        sudo /etc/init.d/php-fpm start
+        sudo /etc/init.d/zoneminder start
         java -jar /app.jar &
         eend $?
 }
@@ -11,5 +15,9 @@ start() {
 stop() {
         ebegin "Stopping $name"
         pkill -f java -jar
+        sudo /etc/init.d/zoneminder stop
+        sudo /etc/init.d/php-fpm stop
+        sudo /etc/init.d/mariadb stop
+        sudo /etc/init.d/lighttpd stop
         eend $?
 }
